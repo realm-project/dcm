3fe5d3a8c65cf06b4bc11d6b36f462e5494a9820
Nathaniel Sherry <nsherry4@uwo.ca>
2015-06-22 12:15:07 -0400

    Messaging: commant in Transcoder

--- messaging/src/main/java/net/realmproject/dcm/messaging/Transcoder.java
+++ messaging/src/main/java/net/realmproject/dcm/messaging/Transcoder.java
@@ -4,6 +4,13 @@ package net.realmproject.dcm.messaging;
 import java.io.Serializable;
 
 
+/**
+ * Interface for transcoding {@link DeviceMessage}s into a different format for
+ * sending/receiving over a messaging implementation
+ * 
+ * @author NAS
+ *
+ */
 public interface Transcoder {
 
     public Serializable encode(DeviceMessage message);
