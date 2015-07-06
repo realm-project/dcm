8bf8e5d715d83f9d6949c6f6b8ddc37cd98ada65
Nathaniel Sherry <nsherry4@uwo.ca>
2015-07-06 14:16:15 -0400

    Added CommandDispatcher class, moved lots of complex dispatch logic out of Commands interface

--- features/src/main/java/net/realmproject/dcm/features/command/Commands.java
+++ features/src/main/java/net/realmproject/dcm/features/command/Commands.java
@@ -1,19 +1,8 @@
 package net.realmproject.dcm.features.command;
 
 
-import java.lang.reflect.Method;
-import java.lang.reflect.Parameter;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.function.Predicate;
-
-import net.realmproject.dcm.event.DeviceEvent;
-import net.realmproject.dcm.event.DeviceEventType;
 import net.realmproject.dcm.event.Logging;
-import net.realmproject.dcm.event.bus.DeviceEventBus;
-import net.realmproject.dcm.event.filter.FilterBuilder;
 import net.realmproject.dcm.event.identity.Identity;
-import net.realmproject.dcm.util.DCMSerialize;
 
 
 /**
@@ -26,20 +15,6 @@ import net.realmproject.dcm.util.DCMSerialize;
  */
 
 public interface Commands extends Identity, Logging {
-
-    default void initCommands(DeviceEventBus bus) {
-
-        Predicate<DeviceEvent> eventFilter = FilterBuilder.start().target(getId()).eventMessage();
-        bus.subscribe(eventFilter, deviceEvent -> {
-            if (deviceEvent.getDeviceEventType() == DeviceEventType.MESSAGE) {
-                Command command = (Command) deviceEvent.getPayload();
-                runCommand(command);
-            }
-        });
-
-        setCommandMethods(generateCommandMethods());
-
-    }
 
     /**
      * This function is called before a {@link CommandMethod} is invoked. If
@@ -67,134 +42,14 @@ public interface Commands extends Identity, Logging {
     default void afterCommand(Command command) {}
 
     default void runCommand(Command command) {
-
-        synchronized (this) {
-            try {
-
-                getLog().info("Device " + getId() + " received command: " + command.getAction());
-                getLog().debug(
-                        getId() + ":" + command.getAction() + ":" + DCMSerialize.serialize(command.getPropertyMap()));
-
-                // Look up method for command
-                Method method = findCommandMethod(command.getAction());
-
-                // case where no method
-                if (method == null) {
-                    getLog().warn(
-                            "Cannot find requested command '" + command.getAction() + "' from available commands");
-                    getLog().warn("Available commands: " + getCommandMethods().keySet());
-                    throw new IllegalArgumentException("Command not found: " + command.getAction());
-                }
-
-                // before anything, call this hook
-                boolean execute = beforeCommand(command);
-                if (!execute) { return; }
-
-                int argCount = method.getParameterCount();
-
-                if (argCount == 0) {
-
-                    setLastCommand(command);
-                    // case where zero-arg method
-                    method.invoke(this, new Object[] {});
-                    return;
-
-                } else if (argCount == 1 && method.getParameters()[0].getAnnotationsByType(Arg.class).length == 0) {
-                    // if there is only 1 argument and there is no Arg
-                    // annotation, try and convert the entire map to the
-                    // expected parameter class type
-
-                    setLastCommand(command);
-                    Object arg = DCMSerialize.convertObject(command.getPropertyMap(), method.getParameterTypes()[0]);
-                    method.invoke(this, arg);
-
-                } else {
-
-                    // case where >1-arg method
-                    Object[] argValues = new Object[argCount];
-
-                    // look up named arguments from top-level map. Then examine
-                    // the
-                    // parameter types, and attempt to convert the data for each
-                    // argument to that type
-                    for (int count = 0; count < argCount; count++) {
-                        Parameter param = method.getParameters()[count];
-                        Arg[] args = param.getAnnotationsByType(Arg.class);
-                        if (args.length == 0) { throw new IllegalArgumentException(
-                                "Named argument calls must have parameters with Arg annotations"); }
-                        Arg arg = args[0];
-                        String paramName = arg.value();
-                        if (!command.getPropertyMap().containsKey(paramName)) {
-                            continue;
-                        }
-
-                        Object argValue = command.getPropertyMap().get(paramName);
-                        argValues[count] = DCMSerialize.convertObject(argValue, method.getParameterTypes()[count]);
-
-                    }
-
-                    setLastCommand(command);
-                    method.invoke(this, argValues);
-
-                }
-
-                // after the method (results may be async), call this hook
-                afterCommand(command);
-
-            }
-            catch (Exception e) {
-                getLog().error("Error executing command " + command.getAction(), e);
-            }
-        }
-
+        getCommandDispatcher().submitCommand(command);
     }
 
-    /*
-     * Start with implementing class, work back up to anything underneath this
-     * to find a CommandMethod this allows subclassing to create devices which
-     * don't need to implement commands directly, but which can overload them if
-     * it chooses.
+    /**
+     * Get the command dispatcher for this Device
+     * 
+     * @return the {@link CommandDispatcher} for this device
      */
-    default Method findCommandMethod(String name) {
-        if (name == null) return null;
-        Map<String, Method> commands = getCommandMethods();
-        if (!commands.containsKey(name)) return null;
-        return commands.get(name);
-    }
-
-    default CommandMethod getCommandMethodAnnotation(Method method) {
-        CommandMethod[] annotations = method.getAnnotationsByType(CommandMethod.class);
-        if (annotations.length == 0) { return null; }
-        return annotations[0];
-    }
-
-    default Map<String, Method> generateCommandMethods() {
-        Map<String, Method> commandMethods = new LinkedHashMap<>();
-        Class<?> cls = this.getClass();
-
-        for (Method m : cls.getMethods()) {
-            if (!m.isAnnotationPresent(CommandMethod.class)) continue;
-            String commandName = getCommandName(m);
-            if (commandMethods.containsKey(commandName)) continue;
-            commandMethods.put(commandName, m);
-        }
-
-        return commandMethods;
-    }
-
-    default String getCommandName(Method m) {
-        CommandMethod cm = m.getAnnotation(CommandMethod.class);
-        if (cm == null) return null;
-        if (cm.value().equals("")) return m.getName();
-        return cm.value();
-    }
-
-    Map<String, Method> getCommandMethods();
-
-    void setCommandMethods(Map<String, Method> methods);
-
-    Command getLastCommand();
-
-    void setLastCommand(Command command);
+    CommandDispatcher getCommandDispatcher();
 
 }
