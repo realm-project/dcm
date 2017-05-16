a56207e6725981cf4a90386664236f18ef2aba5e
Nathaniel Sherry <nsherry4@uwo.ca>
2017-05-16 14:55:06 -0400

    IParcelBranch has both branch resolver anonymous function and a  function for more customizability and ease of use

--- event/src/main/java/net/realmproject/dcm/parcel/impl/flow/IParcelBranch.java
+++ event/src/main/java/net/realmproject/dcm/parcel/impl/flow/IParcelBranch.java
@@ -1,6 +1,6 @@
-package net.realmproject.dcm.parcel.impl.structure;
+package net.realmproject.dcm.parcel.impl.flow;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -11,22 +11,26 @@ import net.realmproject.dcm.parcel.impl.node.IParcelNode;
 
 public class IParcelBranch extends IParcelNode implements ParcelReceiver, ParcelSender {
 
-	private Map<String, ParcelReceiver> receivers = new HashMap<>();
+	protected Map<String, ParcelReceiver> receivers = new LinkedHashMap<>();
 	private Function<Parcel<?>, String> branchResolver = Parcel::getName;
 	
 	public IParcelBranch(Map<String, ParcelReceiver> receivers) {
-		this.receivers = receivers;
+		this.receivers.putAll(receivers);
 	}
 
 	@Override
 	public void send(Parcel<?> parcel) {
 		if (!parcel.visit(getId())) { return; } //cycle detection
-        String branchName = branchResolver.apply(parcel);
+        String branchName = getBranch(parcel);
         if (receivers.keySet().contains(branchName)) {
         	receivers.get(branchName).receive(parcel);
         }
 	}
 
+	protected String getBranch(Parcel<?> parcel) {
+		return branchResolver.apply(parcel);
+	}
+	
 	public Function<Parcel<?>, String> getBranchResolver() {
 		return branchResolver;
 	}
